48c57fca2f13b29c9a01031302971d5622447e22
Feier Zhang <50222087+Fayer-Zhang@users.noreply.github.com>
2021-03-28 23:09:02 -0400

    Add employee menu

---
+++ eHotel/src/Pojo/Customer.java
@@ -0,0 +1,77 @@
+package Pojo;
+
+
+import java.sql.Date;
+
+public class Customer extends AbstractPojo
+{
+    private Integer id;
+    private String  fullName;
+    private String  sinNumber;
+    private String  address;
+    private Date    dateOfRegistration;
+
+    public Customer()
+    {
+    }
+
+    public Customer(Integer id, String fullName, String sinNumber,
+                    String address, Date dateOfRegistration)
+    {
+        this.id                 = id;
+        this.fullName           = fullName;
+        this.sinNumber          = sinNumber;
+        this.address            = address;
+        this.dateOfRegistration = dateOfRegistration;
+    }
+
+    public Integer getId()
+    {
+        return id;
+    }
+
+    public void setId(Integer id)
+    {
+        this.id = id;
+    }
+
+    public String getFullName()
+    {
+        return fullName;
+    }
+
+    public void setFullName(String fullName)
+    {
+        this.fullName = fullName;
+    }
+
+    public String getSinNumber()
+    {
+        return sinNumber;
+    }
+
+    public void setSinNumber(String sinNumber)
+    {
+        this.sinNumber = sinNumber;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public Date getDateOfRegistration()
+    {
+        return dateOfRegistration;
+    }
+
+    public void setDateOfRegistration(Date dateOfRegistration)
+    {
+        this.dateOfRegistration = dateOfRegistration;
+    }
+}
